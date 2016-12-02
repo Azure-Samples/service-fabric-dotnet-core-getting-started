3681796cd6204a65eaa92934563b607fcc68de1a
Prachi <p2607c@gmail.com>
2016-12-02 05:50:17 -0800

    Minor fix in build.sh

--- Actors/CounterActor/build.sh
+++ Actors/CounterActor/build.sh
@@ -3,7 +3,7 @@ DIR=`dirname $0`
 /opt/microsoft/servicefabric/bin/Fabric/Fabric.Code/dotnet/dotnet restore $DIR/../CounterActor/src/CounterActorApplication/CounterActor.Interfaces/project.json -s /opt/microsoft/sdk/servicefabric/csharp/packages -s https://dotnet.myget.org/F/dotnet-core/api/v3/index.json -s https://api.nuget.org/v3/index.json
 /opt/microsoft/servicefabric/bin/Fabric/Fabric.Code/dotnet/dotnet build $DIR/../CounterActor/src/CounterActorApplication/CounterActor.Interfaces/project.json
 
-/opt/microsoft/servicefabric/bin/Fabric/Fabric.Code/dotnet/dotnet restore $DIR/../CounterActor/src/CounterActorApplication/CounterActor/project.json -s /opt/microsoft/sdk/servicefabric/csharp/packagesp -s https://dotnet.myget.org/F/dotnet-core/api/v3/index.json -s https://api.nuget.org/v3/index.json
+/opt/microsoft/servicefabric/bin/Fabric/Fabric.Code/dotnet/dotnet restore $DIR/../CounterActor/src/CounterActorApplication/CounterActor/project.json -s /opt/microsoft/sdk/servicefabric/csharp/packages -s https://dotnet.myget.org/F/dotnet-core/api/v3/index.json -s https://api.nuget.org/v3/index.json
 /opt/microsoft/servicefabric/bin/Fabric/Fabric.Code/dotnet/dotnet build $DIR/../CounterActor/src/CounterActorApplication/CounterActor/project.json
 /opt/microsoft/servicefabric/bin/Fabric/Fabric.Code/dotnet/dotnet publish $DIR/../CounterActor/src/CounterActorApplication/CounterActor/project.json -o $DIR/../CounterActor/CounterActorApplication/CounterActorPkg/Code
 
