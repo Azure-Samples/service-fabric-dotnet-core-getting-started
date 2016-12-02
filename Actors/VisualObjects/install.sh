f08c035a0425a85970216f46dc6a2b1595134403
Hrushikesh Bokil <hrushib@users.noreply.github.com>
2016-12-03 01:26:11 +0530

    Fixed typo in install.sh script

--- Actors/VisualObjects/install.sh
+++ Actors/VisualObjects/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 azure servicefabric application package copy VisualObjectsApplication fabric:ImageStore
-azure servicefabric application type register VisualObjectsApplicationType
+azure servicefabric application type register VisualObjectsApplication
 azure servicefabric application create fabric:/VisualObjectsApplication  VisualObjectsApplicationType 1.0.0
