138f735de5e8ea52953db405cc5b776930c4ed54
Rahul <rapatchi@microsoft.com>
2017-09-14 04:33:21 -0700

    Made changes in build.sh files for TP12

--- Actors/CalculatorActor/build.sh
+++ Actors/CalculatorActor/build.sh
@@ -2,18 +2,18 @@
 DIR=`dirname $0`
 
 cd src/CalculatorActorApplication/CalculatorActor.Interfaces/
-dotnet restore -r ubuntu.16.04-x64 -s /opt/microsoft/sdk/servicefabric/csharp/packages -s https://api.nuget.org/v3/index.json
+dotnet restore -s /opt/microsoft/sdk/servicefabric/csharp/packages -s https://api.nuget.org/v3/index.json
 dotnet build 
 cd -
 
 cd src/CalculatorActorApplication/CalculatorActor/
-dotnet restore -r ubuntu.16.04-x64 -s /opt/microsoft/sdk/servicefabric/csharp/packages -s https://api.nuget.org/v3/index.json
+dotnet restore -s /opt/microsoft/sdk/servicefabric/csharp/packages -s https://api.nuget.org/v3/index.json
 dotnet build 
-dotnet publish --self-contained -r ubuntu.16.04-x64 -o ../../../CalculatorActorApplication/CalculatorActorPkg/Code
+dotnet publish -o ../../../CalculatorActorApplication/CalculatorActorPkg/Code
 cd -
 
 cd src/CalculatorActorApplication/CalculatorActorTestClient/
-dotnet restore -r ubuntu.16.04-x64 -s /opt/microsoft/sdk/servicefabric/csharp/packages -s https://api.nuget.org/v3/index.json
+dotnet restore -s /opt/microsoft/sdk/servicefabric/csharp/packages -s https://api.nuget.org/v3/index.json
 dotnet build 
-dotnet publish --self-contained -r ubuntu.16.04-x64 -o ../../../CalculatorActorTestClient
+dotnet publish -o ../../../CalculatorActorTestClient
 cd -
--- Actors/CounterActor/build.sh
+++ Actors/CounterActor/build.sh
@@ -2,24 +2,24 @@
 DIR=`dirname $0`
 
 cd $DIR/src/CounterActorApplication/CounterActor.Interfaces/
-dotnet restore -r ubuntu.16.04-x64 -s /opt/microsoft/sdk/servicefabric/csharp/packages -s https://api.nuget.org/v3/index.json
+dotnet restore -s /opt/microsoft/sdk/servicefabric/csharp/packages -s https://api.nuget.org/v3/index.json
 dotnet build
 cd -
 
 cd $DIR/src/CounterActorApplication/CounterActor/
-dotnet restore -r ubuntu.16.04-x64 -s /opt/microsoft/sdk/servicefabric/csharp/packages -s https://api.nuget.org/v3/index.json
+dotnet restore -s /opt/microsoft/sdk/servicefabric/csharp/packages -s https://api.nuget.org/v3/index.json
 dotnet build 
-dotnet publish --self-contained -r ubuntu.16.04-x64 -o ../../../CounterActorApplication/CounterActorPkg/Code
+dotnet publish -o ../../../CounterActorApplication/CounterActorPkg/Code
 cd -
 
 cd $DIR/src/CounterActorApplication/CounterActorTestClient/
-dotnet restore -r ubuntu.16.04-x64 -s /opt/microsoft/sdk/servicefabric/csharp/packages -s https://api.nuget.org/v3/index.json
+dotnet restore -s /opt/microsoft/sdk/servicefabric/csharp/packages -s https://api.nuget.org/v3/index.json
 dotnet build 
-dotnet publish --self-contained -r ubuntu.16.04-x64 -o ../../../CounterActorTestClient
+dotnet publish -o ../../../CounterActorTestClient
 cd -
 
 cd $DIR/src/CounterActorApplication/CounterActor.WebService/CounterActorWebService/
-dotnet restore -r ubuntu.16.04-x64 -s /opt/microsoft/sdk/servicefabric/csharp/packages -s https://api.nuget.org/v3/index.json
+dotnet restore -s /opt/microsoft/sdk/servicefabric/csharp/packages -s https://api.nuget.org/v3/index.json
 dotnet build 
-dotnet publish --self-contained -r ubuntu.16.04-x64 -o ../../../../CounterActorApplication/CounterActorWebServicePkg/Code/
+dotnet publish -o ../../../../CounterActorApplication/CounterActorWebServicePkg/Code/
 cd -
