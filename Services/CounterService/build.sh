4693c98cad979426b35930ca71c2aa009837f690
Rahul <rapatchi@microsoft.com>
2017-09-26 16:43:48 +0530

    updated nuget package versions

--- Services/CounterService/build.sh
+++ Services/CounterService/build.sh
@@ -2,19 +2,19 @@
 DIR=`dirname $0`
 
 cd $DIR/src/CounterServiceApplication/CounterService.Interfaces/
-dotnet restore -s /opt/microsoft/sdk/servicefabric/csharp/packages -s https://api.nuget.org/v3/index.json
+dotnet restore -s https://api.nuget.org/v3/index.json
 dotnet build
 cd -
 
 cd $DIR/src/CounterServiceApplication/CounterService/
-dotnet restore -s /opt/microsoft/sdk/servicefabric/csharp/packages -s https://api.nuget.org/v3/index.json
+dotnet restore -s https://api.nuget.org/v3/index.json
 dotnet build 
 dotnet publish -o ../../../CounterServiceApplication/CounterServicePkg/Code
 cd -
 
 
 cd $DIR/src/CounterServiceApplication/CounterService.WebService/CounterServiceWebService/
-dotnet restore -s /opt/microsoft/sdk/servicefabric/csharp/packages -s https://api.nuget.org/v3/index.json
+dotnet restore -s https://api.nuget.org/v3/index.json
 dotnet build 
 dotnet publish -o ../../../../CounterServiceApplication/CounterServiceWebServicePkg/Code/
 cd -
